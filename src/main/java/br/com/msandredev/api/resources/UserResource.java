172424975209bedba78d245728cd63df502b8050
Andre Santos <msandredev@gmail.com>
2022-05-20 00:09:02 -0300

    Add UserService using dependency injection

--- src/main/java/br/com/msandredev/api/resources/UserResource.java
+++ src/main/java/br/com/msandredev/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.msandredev.api.resources;
 
 import br.com.msandredev.api.domain.User;
+import br.com.msandredev.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-
-        return ResponseEntity.ok().body(new User(1, "Andre Santos", "andresantos@gmail.com", "1234"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
