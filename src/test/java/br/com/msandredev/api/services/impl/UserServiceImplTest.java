40400713a0671940f6a203a7ebb86a5612a5920c
Andre Santos <msandredev@gmail.com>
2022-05-25 17:26:47 -0300

    Create unit test for method create when return success

--- src/test/java/br/com/msandredev/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/msandredev/api/services/impl/UserServiceImplTest.java
@@ -16,6 +16,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
@@ -89,7 +90,19 @@ class UserServiceImplTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnSuccess() {
+        when(repository.save(any())).thenReturn(user);
+
+        User response = service.create(userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(FULL_NAME, response.getFullName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
+
+
     }
 
     @Test
