3b54723f5288e528a0be53e08c1d2bb890f2cf6c
Andre Santos <msandredev@gmail.com>
2022-05-27 15:28:19 -0300

    Fix: Set a constructor with no args

--- src/main/java/br/com/msandredev/api/domain/dto/UserDTO.java
+++ src/main/java/br/com/msandredev/api/domain/dto/UserDTO.java
@@ -3,9 +3,11 @@ package br.com.msandredev.api.domain.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
     private Integer id;
