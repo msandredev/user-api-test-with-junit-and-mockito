511ad7b355be3111f6eef0d3c935db335fea93e1
Andre Santos <msandredev@gmail.com>
2022-05-27 15:16:11 -0300

    Remove unused annotations

--- src/main/java/br/com/msandredev/api/domain/dto/UserDTO.java
+++ src/main/java/br/com/msandredev/api/domain/dto/UserDTO.java
@@ -1,17 +1,12 @@
 package br.com.msandredev.api.domain.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.persistence.Column;
 
 @Setter @Getter
 @AllArgsConstructor
-@NoArgsConstructor
 public class UserDTO {
     private Integer id;
     private String fullName;
